974b7b9d6818ab6b92cc933d9c565b80be433e45
mani <sadhak001@gmail.com>
2019-07-12 12:25:20 +0100

    Adding release scripts to repo to create release and tag entries

---
+++ delete-release-from-github.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TARGET_REPO="MeterianHQ/meterian-github-action"
+RELEASE_VERSION="$(cat ${CURRENT_DIR}/version.txt)"
+TAG_NAME="v$(cat ${CURRENT_DIR}/version.txt)"
+
+echo ""
+echo "~~~~ Fetching Release ID for ${TAG_NAME}"
+mkdir -p ${CURRENT_DIR}/artifacts
+CURL_OUTPUT="${CURRENT_DIR}/artifacts/github-release.listing"
+curl \
+    -H "Authorization: token ${METERIAN_GITHUB_TOKEN}" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X GET "https://api.github.com/repos/${TARGET_REPO}/releases/tags/${TAG_NAME}" |
+    tee ${CURL_OUTPUT}
+RELEASE_ID=$(cat ${CURL_OUTPUT} | grep id | head -n 1 | tr -d " " | tr "," ":" | cut -d ":" -f 2)
+
+echo ""
+echo "~~~~ Deleting release with ID ${RELEASE_ID} linked to ${TAG_NAME}"
+curl \
+    -H "Authorization: token ${METERIAN_GITHUB_TOKEN}" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X DELETE "https://api.github.com/repos/${TARGET_REPO}/releases/${RELEASE_ID}"
+
+echo ""
+echo "~~~~ Deleting reference refs/tags/${TAG_NAME}"
+curl \
+    -H "Authorization: token ${METERIAN_GITHUB_TOKEN}" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -X DELETE  "https://api.github.com/repos/${TARGET_REPO}/git/refs/tags/${TAG_NAME}"
---
+++ release-to-github.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TARGET_REPO="MeterianHQ/meterian-github-action"
+
+function createNewRelease() {
+    POST_DATA=$(printf '{
+    "tag_name": "%s",
+    "target_commitish": "master",
+    "name": "%s",
+    "body": "This is a Meterian Scanner GitHub Action, enabling scanning of projects for vulnerabilities. Please provided the necessary METERIAN_API_TOKEN (as a secret key as a repository setting), a token generated via http://meterian.com in order for the action to work. Please checkout the [README](https://github.com/MeterianHQ/meterian-github-action/blob/master/README.md) for further details.",
+    "draft": false,
+    "prerelease": false
+  }' ${TAG_NAME} ${TAG_NAME} ${TAG_NAME})
+  echo "~~~~ Creating release ${RELEASE_VERSION}: $POST_DATA"
+  curl \
+      -H "Authorization: token ${METERIAN_GITHUB_TOKEN}" \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/vnd.github.v3+json" \
+      -X POST -d "${POST_DATA}" "https://api.github.com/repos/${TARGET_REPO}/releases"
+}
+
+if [[ -z ${METERIAN_GITHUB_TOKEN} ]]; then
+  echo "METERIAN_GITHUB_TOKEN cannot be found in the current environment, please populate to proceed either in the startup bash script of your OS or in the environment variable settings of your CI/CD interface."
+  exit -1
+fi
+
+RELEASE_VERSION="$(cat ${CURRENT_DIR}/version.txt)"
+TAG_NAME="v$(cat ${CURRENT_DIR}/version.txt)"
+
+echo "We will be reading version info from the version.txt file, to construct the tagname, please ensure it is the most actual."
+echo "Current TAG_NAME=${TAG_NAME}"
+
+echo ""
+createNewRelease
