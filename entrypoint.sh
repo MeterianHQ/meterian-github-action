e80aef1a902ffd10a7e331cd88cf51c8907acfc8
Nana Insaidoo <nana.insaidoo@meterian.com>
2020-07-10 16:27:08 +0100

    Initial setup for testing meterian/cli-canary image based Docker github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,4 @@ METERIAN_API_TOKEN=${METERIAN_API_TOKEN:?'METERIAN_API_TOKEN missing. Ensure tha
 mv /root/meterian.sh /tmp/meterian.sh
 export METERIAN_CLI_ARGS=$*
 
-# create initialisation script (gradle)
-echo "export PATH=${PATH}" >> /tmp/init.sh
-
-# - add gradle specific configurations
-echo "export GRADLE_HOME=/opt/gradle/gradle-6.1" >> /tmp/init.sh
-echo "export PATH=\${GRADLE_HOME}/bin:\${PATH}" >> /tmp/init.sh
-echo "export GRADLE_USER_HOME=~/.gradle" >> /tmp/init.sh
-echo "" >> /tmp/init.sh
-
-#- add go specific configurations
-echo "export GOROOT=/usr/local/go" >> /tmp/init.sh
-echo "export PATH=\${GOROOT}/bin:\${PATH}" >> /tmp/init.sh
-echo "" >> /tmp/init.sh
-
 /tmp/meterian.sh
