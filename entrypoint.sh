24d66a6a0b9a6701550935a11fca4086eaaab801
Nana Insaidoo <nana.insaidoo@meterian.com>
2020-07-13 10:27:35 +0100

    Updated scripts
    - Now entrypoint.sh starts shell in non-interactive mode training appropriate PATH defined in the Docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/bash
 
 set -e
 set -o pipefail
@@ -7,21 +7,7 @@ set -o pipefail
 METERIAN_API_TOKEN=${METERIAN_API_TOKEN:?'METERIAN_API_TOKEN missing. Ensure that this secret is set correctly.'}
 
 # prepare the script file
-mv /root/meterian.sh /tmp/meterian.sh
+cp /root/meterian.sh /tmp/meterian.sh
 export METERIAN_CLI_ARGS=$*
-
-# create initialisation script (gradle)
-echo "export PATH=${PATH}" >> /tmp/init.sh
-
-# - add gradle specific configurations
-echo "export GRADLE_HOME=/opt/gradle/gradle-6.1" >> /tmp/init.sh
-echo "export PATH=\${GRADLE_HOME}/bin:\${PATH}" >> /tmp/init.sh
-echo "export GRADLE_USER_HOME=~/.gradle" >> /tmp/init.sh
-echo "" >> /tmp/init.sh
-
-#- add go specific configurations
-echo "export GOROOT=/usr/local/go" >> /tmp/init.sh
-echo "export PATH=\${GOROOT}/bin:\${PATH}" >> /tmp/init.sh
-echo "" >> /tmp/init.sh
 
 /tmp/meterian.sh
--- meterian.sh
+++ meterian.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# executing general initialisation script
-#cat /tmp/init.sh
-source /tmp/init.sh
+getLastModifiedDateForFile() {
+	MAYBE_FILE=$1
+
+	WHEN=`date -r $MAYBE_FILE +"%Y-%m-%d" 2>/dev/null`
+	if [[ $? > 0 ]]; then 
+		WHEN='1999-01-01'
+	fi
+
+	# returning the value of $WHEN ( common way of returning data from functions in bash )
+	echo $WHEN
+}
+
+updateClient() {
+	METERIAN_JAR_PATH=$1
+	CLIENT_TARGET_URL=$2
+
+	LOCAL_CLIENT_LAST_MODIFIED_DATE=$(getLastModifiedDateForFile $METERIAN_JAR_PATH)
+	REMOTE_CLIENT_LAST_MODIFIED_DATE=$(date -d "$(curl -s -L -I "${CLIENT_TARGET_URL}" \
+									| grep Last-Modified: | cut -d" " -f2-)" +%F)
+	if [[ "${REMOTE_CLIENT_LAST_MODIFIED_DATE}" > "${LOCAL_CLIENT_LAST_MODIFIED_DATE}" ]];
+	then
+		echo Updating the client$(test -n "${CLIENT_CANARY_FLAG}" && echo " canary" || true)...
+		curl -s -o ${METERIAN_JAR_PATH} "${CLIENT_TARGET_URL}"  >/dev/null
+	fi
+}
 
 # meterian jar location
 METERIAN_JAR=/meterian-cli.jar
 
 # update the client if necessary
-LOCAL_CLIENT_LAST_MODIFIED_DATE=$(date -d "$(ls --full-time ${METERIAN_JAR} | cut -d" " -f6-8)" +%F)
-REMOTE_CLIENT_LAST_MODIFIED_DATE=$(date -d "$(curl -s -L -I "https://www.meterian.com/downloads/meterian-cli.jar" \
-   								   | grep Last-Modified: | cut -d" " -f2-)" +%F)
-if [[ "${REMOTE_CLIENT_LAST_MODIFIED_DATE}" > "${LOCAL_CLIENT_LAST_MODIFIED_DATE}" ]];
-then
-	echo Updating the client...
-	curl -s -o ${METERIAN_JAR} "https://www.meterian.com/downloads/meterian-cli.jar"  >/dev/null
-fi
+updateClient "${METERIAN_JAR}" "https://www.meterian.com/downloads/meterian-cli.jar"
 
-# Printing meterian dockerized version
+# Printing meterian dockerized client version
 cat /root/version.txt 
 
 # launching the client
