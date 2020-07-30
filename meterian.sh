5b76bd4cc192568acd7e6aab4e11c272ebc409f6
Nana Insaidoo <nana.insaidoo@meterian.com>
2020-07-30 10:35:19 +0100

    Now with increased precision in client update function

--- meterian.sh
+++ meterian.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-getLastModifiedDateForFile() {
+getLastModifiedDateTimeForFileInSeconds() {
 	MAYBE_FILE=$1
 
-	WHEN=`date -r $MAYBE_FILE +"%Y-%m-%d" 2>/dev/null`
+	WHEN=`date -r $MAYBE_FILE +"%s" 2>/dev/null`
 	if [[ $? > 0 ]]; then 
-		WHEN='1999-01-01'
+		WHEN="$(date -d "1999-01-01" +%s)"
 	fi
 
 	# returning the value of $WHEN ( common way of returning data from functions in bash )
@@ -52,13 +52,11 @@ updateClient() {
 		CLIENT_TARGET_URL="https://www.meterian.com/downloads/meterian-cli-canary.jar"
 	fi
 
-	LOCAL_CLIENT_LAST_MODIFIED_DATE=$(getLastModifiedDateForFile $METERIAN_JAR_PATH)
-	REMOTE_CLIENT_LAST_MODIFIED_DATE=$(date -d "$(curl -s -L -I "${CLIENT_TARGET_URL}" \
-									| grep Last-Modified: | cut -d" " -f2-)" +%F)
-	if [[ "${REMOTE_CLIENT_LAST_MODIFIED_DATE}" > "${LOCAL_CLIENT_LAST_MODIFIED_DATE}" ]];
+	LOCAL_CLIENT_LAST_MODIFIED_DATE_IN_SECONDS=$(getLastModifiedDateTimeForFileInSeconds $METERIAN_JAR_PATH)
+	REMOTE_CLIENT_LAST_MODIFIED_DATE_IN_SECONDS=$(date -d "$(curl -s -L -I "${CLIENT_TARGET_URL}" | grep Last-Modified: | cut -d" " -f2-)" +%s)
+	if [[ ${REMOTE_CLIENT_LAST_MODIFIED_DATE_IN_SECONDS} -gt ${LOCAL_CLIENT_LAST_MODIFIED_DATE_IN_SECONDS} ]];
 	then
 		echo Updating the client$(test -n "${CLIENT_CANARY_FLAG}" && echo " canary" || true)...
-		rm -f ${METERIAN_JAR_PATH}
 		curl -s -o ${METERIAN_JAR_PATH} "${CLIENT_TARGET_URL}"  >/dev/null
 	fi
 }
